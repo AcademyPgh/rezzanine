8b2c01443de576340041a86216f6ba5cac99051a
Paul Stefko <ham2anv@gmail.com>
2022-12-01 16:21:24 -0500

    Clean reinstall without create-react-app

--- output/index.js
+++ output/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./header";
-
 console.log("help me");
 const header = document.querySelector('#header');
 const root = createRoot(header);
-root.render(<Header />);
+root.render( /*#__PURE__*/React.createElement(Header, null));
